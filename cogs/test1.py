591a7fa7424db5522e122dd5f2e05e9054fb7e6b
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-31 15:26:23 +0900

    Update test1.py

--- cogs/test1.py
+++ cogs/test1.py
@@ -7,5 +7,9 @@ class jinro(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    @commands.command()
+    async def ping(self, ctx):
+        await ctx.send(f"pong! \n`{self.bot.ws.latency * 1000:.0f}ms`")
+
 def setup(bot):
     bot.add_cog(jinro(bot))
