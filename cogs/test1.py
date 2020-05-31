761afa69e87c3d9b3a8119def3ecdde1a00311cc
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-31 15:08:56 +0900

    Update test1.py

--- cogs/test1.py
+++ cogs/test1.py
@@ -1,1 +1,11 @@
+import os
+import r
+from discord.ext import commands
 
+class jinro(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+def setup(bot):
+    bot.add_cog(jinro(bot))
