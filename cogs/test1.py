64f73e8c22b2929058ebee42c74e1424fa17f769
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-31 18:21:57 +0900

    Update test1.py

--- cogs/test1.py
+++ cogs/test1.py
@@ -11,5 +11,41 @@ class jinro(commands.Cog):
     async def ping(self, ctx):
         await ctx.send(f"pong! \n`{self.bot.ws.latency * 1000:.0f}ms`")
 
+    @commands.command(name="参加")
+    async def _add(self, ctx):
+        conn=r.connect()
+        rp=conn.smembers("人狼参加者")
+        pc=0
+        for rpp in rp:
+            rpp=int(rpp)
+            if rpp == ctx.author.id:
+                pc=1
+        if pc==0:
+            p=conn.sadd("人狼参加者",ctx.author.id)
+            if p==True:
+                await ctx.send("参加登録しました！")
+            else:
+                await ctx.send("登録に失敗しました")
+        else:
+            await ctx.send("既に登録されています")
+
+    @commands.command(name="削除")
+    async def _remove(self, ctx):
+        conn=r.connect()
+        rp=conn.smembers("人狼参加者")
+        pc=0
+        for rpp in rp:
+            rpp=int(rpp)
+            if rpp == ctx.author.id:
+                pc=1
+        if pc==1:
+            p=conn.srem("人狼参加者",ctx.author.id)
+            if p==True:
+                await ctx.send("参加削除しました！")
+            else:
+                await ctx.send("削除に失敗しました")
+        else:
+            await ctx.send("参加していません")
+
 def setup(bot):
     bot.add_cog(jinro(bot))
