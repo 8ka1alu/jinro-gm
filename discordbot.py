b2d5c38e91389416509e663528acff68b4cf168b
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-05-31 15:05:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,31 @@
 from discord.ext import commands # Bot Commands Frameworkをインポート
 import os
+import traceback # エラー表示のためにインポート
 
 prefix=os.environ['DISCORD_BOT_PREFIX']
 tokun=os.environ['DISCORD_BOT_TOKEN']
+
+# 読み込むコグの名前を格納しておく。
+INITIAL_EXTENSIONS = 
+[
+    'cogs.test1'
+]
+
 # クラスの定義。ClientのサブクラスであるBotクラスを継承。
 class MyBot(commands.Bot):
+
+    # MyBotのコンストラクタ。
+    def __init__(self, command_prefix):
+        # スーパークラスのコンストラクタに値を渡して実行。
+        super().__init__(command_prefix)
+
+        # INITIAL_COGSに格納されている名前から、コグを読み込む。
+        # エラーが発生した場合は、エラー内容を表示。
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
 
     # Botの準備完了時に呼び出されるイベント
     async def on_ready(self):
