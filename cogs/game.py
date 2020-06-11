43ef673f0ed39b6afebcf332c676ee08c8078a5e
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-06-11 16:24:42 +0900

    Update game.py

--- cogs/game.py
+++ cogs/game.py
@@ -1,1 +1,18 @@
+import os
+import r
+from discord.ext import commands
+import random
 
+class jgame(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def start(self, ctx):
+        conn=r.connect()
+        pp=conn.srem("人狼参加者","0")
+        
+
+def setup(bot):
+    bot.add_cog(jgame(bot))
