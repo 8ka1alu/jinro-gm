b026dbca68e9e93690abf870863880e46d917d27
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-06-17 17:39:56 +0900

    Update roles.py

--- cogs/roles.py
+++ cogs/roles.py
@@ -1,1 +1,63 @@
+import os
+import r
+from discord.ext import commands
+import discord
+import asyncio
 
+class jrole(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.group()
+    async def role(self, ctx):
+        """役職関連"""
+        conn=r.connect()
+        # サブコマンドが指定されていない場合、メッセージを送信する。
+        if ctx.invoked_subcommand is None:
+            jack=conn.smembers("人狼役職")
+            jr=[j for j in jack]          
+            embed=discord.Embed(title="現在人狼役職",description=None)
+            for i in jr:
+                p=conn.get(i)
+                embed.add_field(name=f'**{i}**',value=f'`{p}`')
+            await ctx.send(embed=embed)
+
+    @role.command()
+    async def set(self, ctx, what=None, whats=None):
+        conn=r.connect()
+        kk=conn.smembers("開発管理者")
+        k=[l for l in kk]
+        cai=ctx.author.id
+        cai=str(cai)
+        if cai not in k:
+            return await ctx.send("使用できません")
+        if what==None:
+            return await ctx.send("役職を指定して下さい")
+        if whats==None:
+            return await ctx.send("変更値を指定して下さい")
+        rs=conn.set(what,whats)
+        if rs ==True:
+            embed=discord.Embed(title="役職変更成功",description=None)
+            embed.add_field(name=f'**{what}**',value=f'`{whats}`')
+            await ctx.send(embed=embed)
+        else:
+            embed=discord.Embed(title="役職変更失敗",description="`変更に失敗しました`")
+            await ctx.send(embed=embed)
+
+    @role.command()
+    async def reset(self, ctx, what=None):
+        conn=r.connect()
+        kk=conn.smembers("開発管理者")
+        k=[l for l in kk]
+        cai=ctx.author.id
+        cai=str(cai)
+        if cai not in k:
+            return await ctx.send("使用できません")
+        if what==None:
+            return await ctx.send("役職を指定して下さい")
+        if what=="all":
+            return
+
+def setup(bot):
+    bot.add_cog(jrole(bot))
