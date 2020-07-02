e7c9036a4f36a135702ff114246cae4ec79d17fc
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-07-02 18:38:23 +0900

    Update role_fortune.py

--- cogs/role_fortune.py
+++ cogs/role_fortune.py
@@ -1,1 +1,18 @@
+import os
+import r
+from discord.ext import commands
+import discord
+import random
+import asyncio
 
+class jfortune(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='占い')
+    async def fortune(self, ctx):
+        return
+
+def setup(bot):
+    bot.add_cog(jfortune(bot))
