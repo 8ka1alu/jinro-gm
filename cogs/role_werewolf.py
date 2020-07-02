aed8306519e0f4b595c1fd060ebd0400502ca486
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-07-02 18:50:46 +0900

    Update role_werewolf.py

--- cogs/role_werewolf.py
+++ cogs/role_werewolf.py
@@ -1,1 +1,18 @@
+import os
+import r
+from discord.ext import commands
+import discord
+import random
+import asyncio
 
+class jwerewolf(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='襲撃')
+    async def werewolf(self, ctx):
+        return
+
+def setup(bot):
+    bot.add_cog(jwerewolf(bot))
