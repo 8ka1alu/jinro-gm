60db1b00ecb3ee2b0d25b307c1e5e3f73f580e04
8ka1alu <46132263+8ka1alu@users.noreply.github.com>
2020-07-02 18:49:55 +0900

    Update role_meduim.py

--- cogs/role_meduim.py
+++ cogs/role_meduim.py
@@ -1,1 +1,18 @@
+import os
+import r
+from discord.ext import commands
+import discord
+import random
+import asyncio
 
+class jmeduim(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name='霊媒')
+    async def meduim(self, ctx):
+        return
+
+def setup(bot):
+    bot.add_cog(jmeduim(bot))
